2c81f7e9dab75066e56a91db3f2461f23e3e39e4
JvitorS2 <vssimoesdunck@gmail.com>
2024-10-09 20:57:23 -0300

    inflate triangles

--- src/main/java/br/com/app/Render.java
+++ src/main/java/br/com/app/Render.java
@@ -47,8 +47,11 @@ public class Render {
                         new Vertex(-100, -100, 100),
                         Color.BLUE));
 
+                // Apply 'inflate' to the triangles
+                inflateTriangles(tris);
 
 
+                // Apply rotation XZ matrix to each vertex, heading = headingSlider convert to radians
                 double heading = Math.toRadians(headingSlider.getValue());
                 Matrix3 rotationXZMatriz = new Matrix3(new double[] {
                         Math.cos(heading), 0, -Math.sin(heading),
@@ -56,12 +59,15 @@ public class Render {
                         Math.sin(heading), 0, Math.cos(heading)
                 });
 
+                // Apply rotation YZ matrix to each vertex, pitch = pitchSilder convert to radians
                 double pitch = Math.toRadians(pitchSlider.getValue());
                 Matrix3 rotationYZMatrix = new Matrix3(new double[] {
                         1, 0, 0,
                         0, Math.cos(pitch), Math.sin(pitch),
                         0, -Math.sin(pitch), Math.cos(pitch)
                 });
+
+                // Multiply rotation XZ and YZ
                 Matrix3 rotation = rotationXZMatriz.multiply(rotationYZMatrix);
 
                 BufferedImage img =
@@ -70,8 +76,7 @@ public class Render {
                 double[] zBuffer = new double[img.getWidth() * img.getHeight()];
 
                 //Initialize array with extremely far away depths
-
-                for(int q= 0; q < zBuffer.length; q++) {
+                for(int q = 0; q < zBuffer.length; q++) {
                     zBuffer[q] = Double.NEGATIVE_INFINITY;
                 }
 
@@ -93,8 +98,12 @@ public class Render {
                     System.out.println("V2X: " + v2.x + ",V2Y: " + v2.y );
                     System.out.println("V3X: " + v3.x + ",V3Y: " + v3.y );
 
+                    // this part is similar to a triangle BAC,
+                    //this is AB vertex
                     Vertex ab = new Vertex(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z);
+                    //this is AC vertex
                     Vertex ac = new Vertex(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z);
+                    //this is Vertex normal, perpendicular to abc
                     Vertex norm = new Vertex(
                             ab.y * ac.z - ab.z * ac.y,
                             ab.z * ac.x - ab.x * ac.z,
@@ -103,10 +112,12 @@ public class Render {
 
                     double normalLength =
                             Math.sqrt(norm.x * norm.x + norm.y * norm.y + norm.z * norm.z);
+
                     norm.x /= normalLength;
                     norm.y /= normalLength;
                     norm.z /= normalLength;
 
+                    // this is the angle between the BAC triangle
                     double angleCos = Math.abs(norm.z);
 
                     // compute rectangular bounds for
@@ -114,6 +125,7 @@ public class Render {
                     int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
                     int maxX = (int) Math.min(img.getWidth() - 1,
                             Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+
                     int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
                     int maxY = (int) Math.min(img.getHeight() - 1,
                             Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
@@ -134,7 +146,9 @@ public class Render {
                                 double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
                                 int zIndex = y * img.getWidth() + x;
                                 if(zBuffer[zIndex] < depth){
+                                    // color
                                     img.setRGB(x,y,getShade(t.color,angleCos).getRGB());
+                                    // update z-buffer
                                     zBuffer[zIndex] = depth;
                                 }
 
@@ -146,20 +160,20 @@ public class Render {
 
                 }
 
-
-
                 g2.drawImage(img, 0, 0, null);
             }
         };
         pane.add(renderPanel, BorderLayout.CENTER);
 
+        // Render again the render panel when headingSlider or pitchSlider change
         headingSlider.addChangeListener(e -> renderPanel.repaint());
         pitchSlider.addChangeListener(e -> renderPanel.repaint());
 
-        frame.setSize(400, 400);
+        frame.setSize(800, 800);
         frame.setVisible(true);
     }
 
+    // Create the color
     public static Color getShade(Color color, double shade) {
         double redLinear = Math.pow(color.getRed(), 2.4) * shade;
         double greenLinear = Math.pow(color.getGreen(), 2.4) * shade;
@@ -173,7 +187,43 @@ public class Render {
     }
 
 
+    /*Here's one bonus for you - we can quickly create a sphere approximation from this tetrahedron.
+    It can be done by repeatedly subdividing each triangle into four smaller ones and "inflating"*/
 
+    public static List<Triangle> inflate(List<Triangle> tris) {
+        List<Triangle> result = new ArrayList<>();
+
+        for (Triangle t : tris) {
+            Vertex m1 =
+                    new Vertex((t.v1.x + t.v2.x)/2, (t.v1.y + t.v2.y)/2, (t.v1.z + t.v2.z)/2);
+            Vertex m2 =
+                    new Vertex((t.v2.x + t.v3.x)/2, (t.v2.y + t.v3.y)/2, (t.v2.z + t.v3.z)/2);
+            Vertex m3 =
+                    new Vertex((t.v1.x + t.v3.x)/2, (t.v1.y + t.v3.y)/2, (t.v1.z + t.v3.z)/2);
+
+            result.add(new Triangle(t.v1, m1, m3, t.color));
+            result.add(new Triangle(t.v2, m1, m2, t.color));
+            result.add(new Triangle(t.v3, m2, m3, t.color));
+            result.add(new Triangle(m1, m2, m3, t.color));
+        }
+
+        for (Triangle t : result) {
+            for (Vertex v : new Vertex[] { t.v1, t.v2, t.v3 }) {
+                double l = Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z) / Math.sqrt(30000);
+                v.x /= l;
+                v.y /= l;
+                v.z /= l;
+            }
+        }
+
+        return result;
+    }
+
+    public static void inflateTriangles(List<Triangle> tris){
+        for (int i = 0; i < 4; i++) {
+            tris = inflate(tris);
+        }
+    }
 
 }
 
