5ed430d93f0753a9e24c65db4487565c936d8389
JvitorS2 <vssimoesdunck@gmail.com>
2024-10-09 19:51:03 -0300

    shade

--- src/main/java/br/com/app/Render.java
+++ src/main/java/br/com/app/Render.java
@@ -93,6 +93,22 @@ public class Render {
                     System.out.println("V2X: " + v2.x + ",V2Y: " + v2.y );
                     System.out.println("V3X: " + v3.x + ",V3Y: " + v3.y );
 
+                    Vertex ab = new Vertex(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z);
+                    Vertex ac = new Vertex(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z);
+                    Vertex norm = new Vertex(
+                            ab.y * ac.z - ab.z * ac.y,
+                            ab.z * ac.x - ab.x * ac.z,
+                            ab.x * ac.y - ab.y * ac.x
+                    );
+
+                    double normalLength =
+                            Math.sqrt(norm.x * norm.x + norm.y * norm.y + norm.z * norm.z);
+                    norm.x /= normalLength;
+                    norm.y /= normalLength;
+                    norm.z /= normalLength;
+
+                    double angleCos = Math.abs(norm.z);
+
                     // compute rectangular bounds for
 
                     int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
@@ -118,9 +134,10 @@ public class Render {
                                 double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
                                 int zIndex = y * img.getWidth() + x;
                                 if(zBuffer[zIndex] < depth){
-                                    img.setRGB(x,y,t.color.getRGB());
+                                    img.setRGB(x,y,getShade(t.color,angleCos).getRGB());
                                     zBuffer[zIndex] = depth;
                                 }
+
                             }
 
 
@@ -141,6 +158,18 @@ public class Render {
 
         frame.setSize(400, 400);
         frame.setVisible(true);
+    }
+
+    public static Color getShade(Color color, double shade) {
+        double redLinear = Math.pow(color.getRed(), 2.4) * shade;
+        double greenLinear = Math.pow(color.getGreen(), 2.4) * shade;
+        double blueLinear = Math.pow(color.getBlue(), 2.4) * shade;
+
+        int red = (int) Math.pow(redLinear, 1/2.4);
+        int green = (int) Math.pow(greenLinear, 1/2.4);
+        int blue = (int) Math.pow(blueLinear, 1/2.4);
+
+        return new Color(red, green, blue);
     }
 
 
